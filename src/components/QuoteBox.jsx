714e420c6363600788386bfd7bf070015ae194b8
usrandhe <usrandhe@gmail.com>
2024-05-27 18:57:42 +0530

    font awsome link and Css Fix

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,21 +3,26 @@ import React from 'react';
 const QuoteBox = ({ quote, author, fetchQuote }) => {
   return (
     <div id="quote-box">
-      <p id="text">{quote}</p>
+      <p id="text"><i className="fa fa-quote-left"> </i>{quote}</p>
       <p id="author">- {author}</p>
-      <button id="new-quote" onClick={fetchQuote}>
-        New Quote
-      </button>
-      <a
-        id="tweet-quote"
-        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-          `"${quote}" - ${author}`
-        )}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Tweet
-      </a>
+      <div className="button">
+        <button id="new-quote" className="button" onClick={fetchQuote} 
+        style={{ backgroundColor: 'rgb(44, 62, 80)' }}>
+          New Quote
+        </button>
+        <a className='button' style={{ backgroundColor: 'rgb(44, 62, 80)' }}
+          id="tweet-quote" 
+          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            `"${quote}" - ${author}`
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-twitter"></i>
+
+        </a>
+      </div>
+
     </div>
   );
 };
